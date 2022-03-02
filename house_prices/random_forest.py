0864500172ed71d9d54ca99cee202756c389c80f
unknown <OppenSocial@NoteOppen0831.OPORTUNIDADES.es>
2022-03-02 11:36:05 -0300

    python script

--- house_prices/random_forest.py
+++ house_prices/random_forest.py
@@ -1,63 +1,75 @@
-# Imports
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from turtle import end_fill
+import numpy as np 
+import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
-%matplotlib inline
+import sklearn
+import os
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files 
-# in the input directory
+# diretório
+os.getcwd()
+os.listdir()
+os.chdir('C:\\Users\\OppenSocial\\Desktop\\kaggle_competitions\\house_prices')
 
-from subprocess import check_output
-print(check_output(["ls", "../input"]).decode("utf8"))
-
-# Any results you write to the current directory are saved as output.
-
-# Import the train and test set
-train = pd.read_csv('../input/train.csv')
-test = pd.read_csv('../input/test.csv')
+# importando os dados
+train = pd.read_csv('./train.csv')
+test = pd.read_csv('./test.csv')
 train.head()
 
+# agrupando os dados
+ntrain = train.shape[0]
+ntest = test.shape[0]
+y_train = train.SalePrice.values
+all_data = pd.concat((train, test)).reset_index(drop = True)
+# retirando a coluna y
+all_data.drop(['SalePrice'], axis = 1, inplace = True)
 
-# Handle missing values
-train.isnull().sum()[train.isnull().sum()>0]
+# nomes das variáveis
+all_cols = all_data.columns
 
-train = train.drop(['PoolQC','Fence','MiscFeature','Alley','FireplaceQu'], axis=1)
-test = test.drop(['PoolQC','Fence','MiscFeature','Alley','FireplaceQu'], axis=1)
+# nomes das colunas numéricas
+num_cols = all_data._get_numeric_data().columns
 
+# nomes das colunas categóricas
+cat_cols = list(set(all_cols) -  set(num_cols))
 
-# Select columns with a correlation > 0.5
-rel_vars = corr.SalePrice[(corr.SalePrice > 0.5)]
-rel_cols = list(rel_vars.index.values)
+# contagem de missing
+df_missing = all_data.isnull().sum(axis = 0).reset_index()
+df_missing.columns = ['column_name', 'missing']
+print(df_missing[df_missing['missing'] > 0])
 
-corr2 = train[rel_cols].corr()
-plt.figure(figsize=(8,8))
-hm = sns.heatmap(corr2, annot=True, annot_kws={'size':10})
-plt.yticks(rotation=0, size=10)
-plt.xticks(rotation=90, size=10)
-plt.show()
+# input de medianos em NAs (num_cols)
+for cols in num_cols:
+    all_data[cols] = all_data[cols].fillna(all_data[cols].median())
 
-# Create matrix with independent variables
-X = train[rel_cols[:-1]].iloc[:,0:].values
-y = train.iloc[:, -1].values
+# criando categoria para NAs = missing (cat_cols)
+for cols in cat_cols:
+    all_data[cols] = all_data[cols].fillna('missing')
 
-# Create training and test dataset
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state = 0)
+# conferindo se ainda existe algum NA
+all_data.isnull().values.sum()
 
-# Fit Random Forest on Training Set
+# categórica para números
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+for cols in cat_cols:
+    all_data[cols] = le.fit_transform(all_data[cols])
+
+# dividindo em dados de teste e treino novamente:
+X_train = all_data[:train.shape[0]]
+X_test = all_data[train.shape[0]:]
+
+# Random Forest
 from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators=300, random_state=0)
+regressor = RandomForestRegressor(n_estimators = 150, random_state = 0, oob_score = True)
 regressor.fit(X_train, y_train)
 
-# Score model
-regressor.score(X_train, y_train)
-
-# Predict new result
+# previsão:
 y_pred = regressor.predict(X_test)
-# Plot y_test vs y_pred
-plt.figure(figsize=(12,8))
-plt.plot(y_test, color='red')
-plt.plot(y_pred, color='blue')
+# y de teste e y predito
+plt.figure(figsize = (12))
+plt.plot(y_test, color = 'red')
+plt.plot(y_pred, color = 'blue')
 plt.show()
+
+
